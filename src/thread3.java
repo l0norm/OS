d2e7d09af15d690bc5d0c4ad95d9c72a64bf1727
Fahad Alharbi <ff7rbi04@gmail.com>
2025-03-09 06:16:26 +0300

    second thread shittiest code

--- src/thread3.java
+++ src/thread3.java
@@ -24,22 +24,53 @@ public class thread3 implements Runnable{
             StringBuilder processID = new StringBuilder();
             StringBuilder burstTimeEnd = new StringBuilder("0");
 
-            int burstTime = 0;
+            int totalBurstTime = 0;
             while(!readyQ.isEmpty()){
                 PCB pcb = new PCB(readyQ.peek());
-                burstTime+=pcb.burstTime;
+                totalBurstTime+=pcb.burstTime;
                 chart.append("+").append("-".repeat(pcb.burstTime));
                 processID.append("|").append(" ".repeat(pcb.burstTime));
-                burstTimeEnd.append(" ".repeat(pcb.burstTime)).append(burstTime);
+                burstTimeEnd.append(" ".repeat(pcb.burstTime)).append(totalBurstTime);
 
 
             }
         }else if(this.algType.equals("RR")){
+            StringBuilder chart = new StringBuilder();
+            StringBuilder processID = new StringBuilder();
+            StringBuilder burstTimeEnd = new StringBuilder("0");
 
+            int quantum = 7;
+            int totalBurstTime = 0;
+            while(!readyQ.isEmpty()){
+                PCB pcb = new PCB(readyQ.poll());
+
+                
+                pcb.burstTime -=quantum;
+                int bursted;
+                if(pcb.burstTime<=0){
+                    bursted = pcb.burstTime;
+                    pcb.burstTime = 0;
+
+                }else{
+                    readyQ.add(pcb);
+                    bursted = quantum;
+                }
+                totalBurstTime+=bursted;
+
+                chart.append("+").append("-".repeat(bursted));
+                processID.append("|").append(" ".repeat(bursted));
+                burstTimeEnd.append(" ".repeat(bursted)).append(totalBurstTime);
+
+            }
 
 
 
         }else if(algType=="priorityScheduling"){
+            StringBuilder chart = new StringBuilder();
+            StringBuilder processID = new StringBuilder();
+            StringBuilder burstTimeEnd = new StringBuilder("0");
+
+            
 
         }
     }
